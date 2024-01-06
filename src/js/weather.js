ae44ec8a69be40c5c0e8c783d1a3dff139c5e735
nikolasohigh <kolya.bondar.yt@gmail.com>
2024-01-06 12:54:00 +0100

    init and test commit

---
+++ src/js/weather.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.weather__carousel').slick({
+       infinite: false,
+       slidesToShow: 5,
+       slidesToScroll: 5,
+       arrows: false,
+       autoplay: true,
+       autoplaySpeed: 5000
+    });
+});
